56e31aad919da62914ec9ac23de93ff85651a2dc
MatMa4 <mateomanassero.4@gmail.com>
2025-10-03 15:47:18 -0300

    algunas cosas

--- TP/src/main/java/DAOs/HuespedDAO.java
+++ TP/src/main/java/DAOs/HuespedDAO.java
@@ -2,12 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
  */
-package dominio;
+package DAOs;
+import dominio.Huesped;
+import repositorio.HuespedDTO;
 
 /**
  *
  * @author mateo
  */
 public interface HuespedDAO {
-    
+    public HuespedDTO consultarDocumento(String tipoDocumento, String numeroDocumento);
+    public void modificarHuesped(HuespedDTO huespedModificado, Huesped huespedAntiguo);
+
 }
