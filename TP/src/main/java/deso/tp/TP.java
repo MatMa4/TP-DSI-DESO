2ee499cb7e5db653e8867ebba1445a97785e897e
MatMa4 <mateomanassero.4@gmail.com>
2025-10-12 01:36:43 -0300

    odio git

--- TP/src/main/java/deso/tp/TP.java
+++ TP/src/main/java/deso/tp/TP.java
@@ -3,22 +3,7 @@
  */
 
 package deso.tp;
-import java.time.LocalDate;
-
-import DAOs.HuespedDAO;
-import DAOs.HuespedDAOImpl;
-import dominio.Huesped;
-import dominio.Direccion;
-import repositorio.HuespedDTO;
-<<<<<<< HEAD
 import servicio.CU10;
-=======
-import DAOs.UsuarioDAOImpl;
-import Excepcion.ContrasenaInvalidaException;
-import Excepcion.UsuarioNoEncontradoException;
-import repositorio.UsuarioDTO;
-import servicios.GestorDeUsuario;
->>>>>>> 57d98d41da18ece2efaea1e2bc72e308b561fd62
 
 /**
  *
@@ -27,37 +12,7 @@ import servicios.GestorDeUsuario;
 public class TP {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
         CU10.main(args);
-=======
-        HuespedDAOImpl huespedDAO= HuespedDAOImpl.getHuespedDAO();
-        Direccion direccion = new Direccion("Calle Falsa", "123", "D", 23, 62704, "Springfield", "Illinois", "USA");
-        Huesped huesped = new Huesped.Builder()
-            .apellido("Gómez")
-            .nombre("Mateo")
-            .tipoDocumento("DNI")
-            .numeroDocumento("12345678")
-            .fechaNacimiento(LocalDate.of(1990, 5, 20))
-            .direccionHuesped(direccion)
-            .telefono("3421234567")
-            .ocupacion("Ingeniero")
-            .nacionalidad("Argentina")
-            .email("mateo.gomez@example.com") // opcional
-            .cuit("20-12345678-9")            // opcional
-            .posicionIVA("Responsable Inscripto") // opcional, si no se pone se asigna "Consumidor Final"
-            .build();
-
-
-        huespedDAO.guardarHuesped(huesped);
-        HuespedDTO dto = huespedDAO.consultarDocumento("DNI", "12345678");
-        System.out.println("Huésped encontrado: " + dto.getNombre() + " " + dto.getApellido());
-        System.out.println("Hola");
-        
-        GestorDeUsuario gestor = new GestorDeUsuario(new UsuarioDAOImpl("src/main/java/BDD/info.csv"));
-        UsuarioDTO userDto = new UsuarioDTO("juan","1234");
-        gestor.autenticarUsuario(userDto);
-        System.out.print("se pudo");
->>>>>>> 57d98d41da18ece2efaea1e2bc72e308b561fd62
 
     }
 }
