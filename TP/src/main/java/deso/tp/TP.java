bcad97bedda030b02735021f1958c4bc18932328
MatMa4 <mateomanassero.4@gmail.com>
2025-10-12 00:50:25 -0300

    cambios en CU10, CU11, UI-CU10 y algunas cosas mas

--- TP/src/main/java/deso/tp/TP.java
+++ TP/src/main/java/deso/tp/TP.java
@@ -11,6 +11,7 @@ import DAOs.HuespedDAOImpl;
 import dominio.Huesped;
 import dominio.Direccion;
 import repositorio.HuespedDTO;
+import servicio.CU10;
 
 /**
  *
@@ -19,27 +20,7 @@ import repositorio.HuespedDTO;
 public class TP {
 
     public static void main(String[] args) {
-        HuespedDAOImpl huespedDAO= HuespedDAOImpl.getHuespedDAO();
-        Direccion direccion = new Direccion("Calle Falsa", "123", "D", 23, 62704, "Springfield", "Illinois", "USA");
-        Huesped huesped = new Huesped.Builder()
-            .apellido("Gómez")
-            .nombre("Mateo")
-            .tipoDocumento("DNI")
-            .numeroDocumento("12345678")
-            .fechaNacimiento(LocalDate.of(1990, 5, 20))
-            .direccionHuesped(direccion)
-            .telefono("3421234567")
-            .ocupacion("Ingeniero")
-            .nacionalidad("Argentina")
-            .email("mateo.gomez@example.com") // opcional
-            .cuit("20-12345678-9")            // opcional
-            .posicionIVA("Responsable Inscripto") // opcional, si no se pone se asigna "Consumidor Final"
-            .build();
-
-
-        huespedDAO.guardarHuesped(huesped);
-        HuespedDTO dto = huespedDAO.consultarDocumento("DNI", "12345678");
-        System.out.println("Huésped encontrado: " + dto.getNombre() + " " + dto.getApellido());
+        CU10.main(args);
 
     }
 }
