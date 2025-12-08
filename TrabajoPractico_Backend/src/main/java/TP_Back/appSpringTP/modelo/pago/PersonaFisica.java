ee6f50bdbaa7f9c4215b75b57870017a99b8237c
SebDev <navarros0212@gmail.com>
2025-12-07 21:22:10 -0300

    Modifica persona fisica

--- TrabajoPractico_Backend/src/main/java/TP_Back/appSpringTP/modelo/pago/PersonaFisica.java
+++ TrabajoPractico_Backend/src/main/java/TP_Back/appSpringTP/modelo/pago/PersonaFisica.java
@@ -4,10 +4,29 @@
  */
 package TP_Back.appSpringTP.modelo.pago;
 
+import TP_Back.appSpringTP.modelo.huesped.Huesped;
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  *
  * @author mateo
  */
+@Entity
+@DiscriminatorValue("PersonaFisica")
+@Getter
+@Setter
 public class PersonaFisica extends ResponsableDePago{
+    private Integer id_per_fisica;
     
+    @OneToOne
+    @jakarta.persistence.JoinColumns({
+        @JoinColumn(name = "huesped_numero_documento", referencedColumnName = "numeroDocumento"),
+        @JoinColumn(name = "huesped_tipo_documento", referencedColumnName = "tipoDocumento")
+    })
+    private Huesped huesped;
 }
