2facd209369e6b191575003284d7633d6d971068
MatMa4 <mateomanassero.4@gmail.com>
2025-12-09 11:07:34 -0300

    nada

--- TrabajoPractico_Backend/src/main/java/TP_Back/appSpringTP/gestores/GestorDeHabitaciones.java
+++ TrabajoPractico_Backend/src/main/java/TP_Back/appSpringTP/gestores/GestorDeHabitaciones.java
@@ -4,6 +4,12 @@
  */
 package TP_Back.appSpringTP.gestores;
 
+import TP_Back.appSpringTP.DAOs.HabitacionDAO;
+import TP_Back.appSpringTP.DTOs.HabitacionDetalleDTO;
+import java.util.Date;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,6 +17,12 @@ import org.springframework.stereotype.Service;
  * @author mateo
  */
 @Service
+@AllArgsConstructor
 public class GestorDeHabitaciones {
+    @Autowired
+    private final HabitacionDAO habitacionDAO;
     
+    public List<HabitacionDetalleDTO> getHabitacionesConDetalle(Date fechaInicio, Date fechaFin){
+        return habitacionDAO.getHabitacionesConDetalle(fechaInicio, fechaFin);
+    }
 }
