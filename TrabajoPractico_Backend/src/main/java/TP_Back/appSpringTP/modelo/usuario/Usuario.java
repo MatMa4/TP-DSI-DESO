416f9b47b7a9ac6d7ee206949833cbc5dc21cede
MatMa4 <mateomanassero.4@gmail.com>
2025-12-16 17:49:43 -0300

    test unitarios de gestor de usuarios

--- TrabajoPractico_Backend/src/main/java/TP_Back/appSpringTP/modelo/usuario/Usuario.java
+++ TrabajoPractico_Backend/src/main/java/TP_Back/appSpringTP/modelo/usuario/Usuario.java
@@ -4,9 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "usuarios")
+@Getter
+@Setter
 public class Usuario {
     @Id
     @Column(name = "usuario", length = 50, nullable = false)
