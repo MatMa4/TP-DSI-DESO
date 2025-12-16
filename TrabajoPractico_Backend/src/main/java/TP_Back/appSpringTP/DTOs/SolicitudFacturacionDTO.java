c74bfee15996b2d01f8c61288ac73a2c40839ce9
SebDev <navarros0212@gmail.com>
2025-12-16 09:51:07 -0300

    Crea test unitarios para Gestor de Facturas

--- TrabajoPractico_Backend/src/main/java/TP_Back/appSpringTP/DTOs/SolicitudFacturacionDTO.java
+++ TrabajoPractico_Backend/src/main/java/TP_Back/appSpringTP/DTOs/SolicitudFacturacionDTO.java
@@ -2,11 +2,16 @@ package TP_Back.appSpringTP.DTOs;
 
 import TP_Back.appSpringTP.DTOs.ocupacion.ConsumoDTO;
 import java.util.List;
+
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class SolicitudFacturacionDTO {
     private Long idOcupacion;
     private List<ConsumoDTO> listaConsumos;
