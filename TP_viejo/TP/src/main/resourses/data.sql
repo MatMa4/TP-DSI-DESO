45a02e8e8ecaf8037b4223be033e06a57580d0df
MatMa4 <mateomanassero.4@gmail.com>
2025-12-16 21:53:27 -0300

    TP mas limpio, eliminado archivos innecesarios

--- TP_viejo/TP/src/main/resourses/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO huesped (name, contact) VALUES ('John Doe', 'john@example.com');
-INSERT INTO huesped (name, contact) VALUES ('Jane Smith', 'jane@example.com');
-INSERT INTO huesped (name, contact) VALUES ('Alice Johnson', 'alice@example.com');
-INSERT INTO huesped (name, contact) VALUES ('Bob Brown', 'bob@example.com');
